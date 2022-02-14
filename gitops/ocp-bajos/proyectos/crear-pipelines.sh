0d3be447eee675b833ef49b5febc1e67a0b54bdd
Agustin Lago <alago@redhat.com>
2022-02-14 10:16:03 -0300

    test script

--- gitops/ocp-bajos/proyectos/crear-pipelines.sh
+++ gitops/ocp-bajos/proyectos/crear-pipelines.sh
@@ -2,47 +2,8 @@ echo "Iniciando"
 cluster=$(oc project) &> /dev/null
 TOKEN=${COMPLETAR CON EL TOKEN}
 URL_CLUSTER=${COMPLETAR URL DEL CLUSTER}
-case $cluster in
-            *"$URL_CLUSTER_PROD"* )
-                echo "Se encuentra logueado a PROD, se va a usar la SA Generica.."
-                oc login --server=$URL_CLUSTER --token=$TOKEN &> /dev/null
-                LOG=0
-            ;;
-            *"$URL_CLUSTER"* )
-                echo "Se encuentra logueado a DEVQA, se va a usar el usuario: $(oc whoami)"
-                LOG=1
-            ;;
-            *"No project has been set. Pass a project name to make that the default."* )
-                echo "No se puede determinar, se va a usar la SA Generica"
-                oc login --server=$URL_CLUSTER --token=$TOKEN &> /dev/null
-                LOG=0
-            ;;
-            * )
-                echo "No estas logueado en OCP, se va a usar la SA generica"
-                oc login --server=$URL_CLUSTER --token=$TOKEN &> /dev/null
-                LOG=0
-            ;;
-esac
-
-read -p "Ingrese el nombre del Modulo o dejelo vacio sino se usa: " modulo
-
-if [ -z $modulo ]
-then
-    MODULO='/'
-    echo "Se va a crear un proyecto sin modulo"
-else
-    if [ -d $modulo ]
-    then
-        echo "Se va a usar el modulo: '$modulo'"
-    else
-        echo "El modulo no existe!"
-        exit
-    fi
-    
-
-
-    MODULO='/'$modulo'/'
-fi
+echo "Se va a usar la SA generica"
+oc login --server=$URL_CLUSTER --token=$TOKEN &> /dev/null
 
 read -p "Ingrese el nombre del Base-Project (Namespace sin el entorno): " project
 
@@ -51,7 +12,7 @@ then
     echo "Falta el parametro BASE-PROJECT"
     exit
 else
-    if [ -d .$MODULO$project ]
+    if [ -d $project ]
     then
         echo "Se va a usar el proyecto: '$project'"
     else
@@ -83,29 +44,49 @@ else
     TECH=$tech
 fi
 
+read -p "Ingrese el repo de codigo de la Aplicacion: " repo_codigo
+
+if [ -z $repo_codigo ]
+then
+    echo "Falta el parametro GIT_REPO"
+    exit
+else
+    echo "Se va a usar el repo: '$repo_codigo'"
+    GIT_REPO=$repo_codigo
+fi
+
+read -p "Ingrese el repo de codigo de la Aplicacion: " repo_manifests
+
+if [ -z $repo_manifests ]
+then
+    echo "Falta el parametro GIT_REPO"
+    exit
+else
+    echo "Se va a usar el repo: '$repo_manifests'"
+    GIT_KUSTOMIZE=$repo_manifests
+fi
+
 echo "Procesando.."
 
 #APP_NAME=snvs-cuidarfile-sheduler
 #BASE_PROJECT=snvs-ws-api
 #MODULO=snvs
 #TECH=java
-GIT_REPO=ssh://git@10.10.103.100${MODULO}${BASE_PROJECT}/${APP_NAME}.git
-GIT_KUSTOMIZE=ssh://git@10.10.103.100/DevOps/manifests${MODULO}${BASE_PROJECT}/${APP_NAME}.git
+#GIT_REPO=ssh://git@10.10.103.100/${BASE_PROJECT}/${APP_NAME}.git
+#GIT_KUSTOMIZE=https://github.com/agustinlago/devops/DevOps/manifests/${BASE_PROJECT}/${APP_NAME}.git
 
 oc process repository-template -n openshift -o yaml \
     --param APP_NAME=$APP_NAME \
-    --param BASE_PROJECT=$BASE_PROJECT \
-    --param MODULO=$MODULO > ../../repositories$MODULO$BASE_PROJECT/${APP_NAME}-repo.yaml
+    --param BASE_PROJECT=$BASE_PROJECT  > ../../repositories/$BASE_PROJECT/${APP_NAME}-repo.yaml
 
 oc process ${TECH}-ci-pipeline-template -n openshift -o yaml \
     --param APP_NAME=${APP_NAME} \
     --param BASE_PROJECT=${BASE_PROJECT} \
-    --param GIT_REPO=${GIT_REPO} > .$MODULO${BASE_PROJECT}/dev/pipelines/${APP_NAME}-${TECH}-ci-pipeline.yaml
+    --param GIT_REPO=${GIT_REPO} > ${BASE_PROJECT}/dev/pipelines/${APP_NAME}-${TECH}-ci-pipeline.yaml
 
 oc process dev-app-template -n openshift -o yaml \
     --param APP_NAME=$APP_NAME \
-    --param BASE_PROJECT=$BASE_PROJECT \
-    --param MODULO=$MODULO  > .$MODULO$BASE_PROJECT/dev/apps/${APP_NAME}-app.yaml
+    --param BASE_PROJECT=$BASE_PROJECT > $BASE_PROJECT/dev/apps/${APP_NAME}-app.yaml
 
 
 
@@ -113,32 +94,25 @@ oc process ${TECH}-cd-pipeline-template -n openshift -o yaml \
     --param APP_NAME=${APP_NAME} \
     --param BASE_PROJECT=${BASE_PROJECT} \
     --param GIT_REPO=${GIT_REPO} \
-    --param GIT_KUSTOMIZE=${GIT_KUSTOMIZE} > .$MODULO${BASE_PROJECT}/qa/pipelines/${APP_NAME}-${TECH}-cd-pipeline.yaml
+    --param GIT_KUSTOMIZE=${GIT_KUSTOMIZE} > ${BASE_PROJECT}/qa/pipelines/${APP_NAME}-${TECH}-cd-pipeline.yaml
 
 oc process qa-app-template -n openshift -o yaml \
     --param APP_NAME=$APP_NAME \
-    --param BASE_PROJECT=$BASE_PROJECT \
-    --param MODULO=$MODULO > .$MODULO$BASE_PROJECT/qa/apps/${APP_NAME}-app.yaml
+    --param BASE_PROJECT=$BASE_PROJECT > $BASE_PROJECT/qa/apps/${APP_NAME}-app.yaml
 
 
 
 oc process manifest-cd-pipeline-template -n openshift -o yaml \
     --param APP_NAME=${APP_NAME} \
     --param BASE_PROJECT=${BASE_PROJECT} \
-    --param GIT_KUSTOMIZE=${GIT_KUSTOMIZE} > .$MODULO${BASE_PROJECT}/uat/pipelines/${APP_NAME}-manifest-cd-pipeline.yaml
+    --param GIT_KUSTOMIZE=${GIT_KUSTOMIZE} > ${BASE_PROJECT}/uat/pipelines/${APP_NAME}-manifest-cd-pipeline.yaml
 
 oc process uat-app-template -n openshift -o yaml \
     --param APP_NAME=$APP_NAME \
-    --param BASE_PROJECT=$BASE_PROJECT \
-    --param MODULO=$MODULO > .$MODULO$BASE_PROJECT/uat/apps/${APP_NAME}-app.yaml
+    --param BASE_PROJECT=$BASE_PROJECT > $BASE_PROJECT/uat/apps/${APP_NAME}-app.yaml
 
-
-
-if [ $LOG -eq 0 ]
-then
-    echo "Se va a desloguear de OCP"
-    oc logout &> /dev/null
-fi
+echo "Se va a desloguear de OCP"
+oc logout &> /dev/null
 
 echo "Finalizado con exito!"
 
